ac04f8b77c7206291ac8deefb3ce1e1ef2b42e9c
Zero <quoctrung.tn29@gmail.com>
2023-01-07 10:25:28 +0700

    fix: fix detect

--- detect.py
+++ detect.py
@@ -186,7 +186,7 @@ def detect(save_img=False, opt = {}):
 
 def detect_export(path):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp/weights/best.pt',
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp/weights/best-tiny.pt',
                         help='model.pt path(s)')
     parser.add_argument('--source', type=str, default=path,
                         help='source')  # file/folder, 0 for webcam
@@ -217,41 +217,6 @@ def detect_export(path):
                 strip_optimizer(opt.weights)
         else:
             detect(False, opt)
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp/weights/best.pt', help='model.pt path(s)')
-# parser.add_argument('--source', type=str, default='inference/images/lp1.jpg', help='source')  # file/folder, 0 for webcam
-# parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-# parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-# parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-# parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-# parser.add_argument('--view-img', action='store_true', help='display results')
-# parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-# parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-# parser.add_argument('--save-crop',action='store_true',default=True,help='save crop prediction box')
-# parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-# parser.add_argument('--classes', nargs='+', type=int,default=[0,1,2],help='filter by class: --class 0, or --class 0 1 2')
-# parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-# parser.add_argument('--augment', action='store_true', help='augmented inference')
-# parser.add_argument('--update', action='store_true', help='update all models')
-# parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-# parser.add_argument('--name', default='exp', help='save results to project/name')
-# parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-# parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
-# opt = parser.parse_args()
-# print(opt)
-# #check_requirements(exclude=('pycocotools', 'thop'))
-#
-# with torch.no_grad():
-#     if opt.update:  # update all models (to fix SourceChangeWarning)
-#         for opt.weights in ['yolov7.pt']:
-#             detect()
-#             strip_optimizer(opt.weights)
-#     else:
-#         detect()
-
-
-
 
 
 # if __name__ == '__main__':
